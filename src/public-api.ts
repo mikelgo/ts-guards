96c0eab0ab69709405e6ac0b2feae1af4ff40fa4
mikelgo <m.gonser@icloud.com>
2020-12-10 11:07:38 +0100

    release configuration

---
+++ src/public-api.ts
@@ -0,0 +1,5 @@
+export {isArray} from "./is-array";
+export {isBoolean} from "./is-boolean";
+export {isNumber} from "./is-number";
+export {isString} from "./is-string";
+export {isObject} from "./is-object";
